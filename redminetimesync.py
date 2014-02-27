c0ce881f7590a3bdd4c6de1d8c80316f834de226
Bruno Duyé <bduye@easter-eggs.com>
2014-02-27 15:41:12 +0100

    Add option to use Hamster categories for matching Redmine activities

--- redminetimesync.py
+++ redminetimesync.py
@@ -6,6 +6,8 @@ import sys
 import ConfigParser
 import re
 from xml.dom import minidom
+import yaml
+
 from redmine import Redmine
 
 
@@ -28,15 +30,16 @@ def fetchFromDatabase(dataFile, date):
     '''Following http://docs.python.org/library/sqlite3.html
     Fetch data from an SQLITE3 database
     Returns an iterable object with SELECT result'''
-    _date = ("%"+date+"%", "%"+date+"%") # check that we get just today
+    _date = "%{}%".format(date)  # check that we get just today
     connection = sqlite3.connect(os.path.expanduser(dataFile))
     dbCursor = connection.cursor()
     dbCursor.execute("""SELECT
-            activities.name,facts.start_time,facts.end_time,facts.description
+            activities.name,facts.start_time,facts.end_time,facts.description,categories.name
             FROM activities
             JOIN facts ON activities.id = facts.activity_id
+            LEFT JOIN categories ON activities.category_id = categories.id
             WHERE facts.start_time LIKE ?
-            AND facts.end_time LIKE ?""", _date)
+            AND facts.end_time LIKE ?""", (_date, _date))
     return dbCursor
 
 def calDuration(t2,t1):
@@ -47,40 +50,73 @@ def calDuration(t2,t1):
     duration = datetime.timedelta(0,int(t2[2]),0,0,int(t2[1]),int(t2[0])) - datetime.timedelta(0,int(t1[2]),0,0,int(t1[1]),int(t1[0]))
     return round(duration.seconds/3600.0, 1)
 
-def getTimeEntries(time_entries):
+def getTimeEntries(time_entries, verbose=True):
     '''Return an array of explicit associative array for times entries, filtering out
     entries that do not match issue_id_regexp defined in config file'''
+    categories_association = yaml.load(open("/home/bruno/dev/redminetimesync/activities.config", 'r'))
+    if configProperties.has_option('default', 'redmine_default_activity_id'):
+        default_activity_id = configProperties.get('default', 'redmine_default_activity_id')
+    else:
+        default_activity_id = None
     array = []
     for time_entry in time_entries:
         label = time_entry[0]
+        duration = calDuration(time_entry[2], time_entry[1])
         # Try to find Redmine issue IDs from label using regexp defined in config file
         match = re.match(configProperties.get('default', 'issue_id_regexp'), label)
         if match:
-            array.append({
-                'label': label,
-                'issue_id': match.group(1),
-                'duration': calDuration(time_entry[2], time_entry[1]),
-                'comment': time_entry[3]})
+            issue_id = match.group(1)
         else:
             print u'** Warning : ignoring entry "{}" : not able to find issue ID'.format(label)
+            continue
+        print u"* [{duration}h] #{id} : {label}".format(
+            duration=duration, id=issue_id, label=label
+        )
+        # Try to find activity_id
+        category_name = time_entry[4]
+        if category_name is not None and categories_association is not None:
+            if category_name in categories_association:
+                activity_id = categories_association[category_name]
+            else:
+                print_(u'** Warning : unmatched category "{}"'.format(category_name))
+                if default_activity_id is not None:
+                    print u', assignated to default Redmine activity ID : {}'.format(activity_id)
+                    activity_id = default_activity_id
+                else:
+                    activity_id = None
+                print '\n'
+        else:
+            if default_activity_id is not None:
+                activity_id = default_activity_id
+            else:
+                activity_id = None
+
+        array.append({
+            'description': label,
+            'label': label,
+            'issue_id': issue_id,
+            'duration': duration,
+            'comment': time_entry[3],
+            'activity_id': activity_id
+            })
     return array
 
 def generateXml(time_entries, date):
     '''Takes time entries and generate an xml good for Redmine APIs
     Returns a string with the parsable XML
     Cfr: http://www.redmine.org/projects/redmine/wiki/Rest_TimeEntries'''
+
+    def empty_if_none(string):
+        return string if string is not None else ''
+
     myxml = []
     for time_entry in time_entries:
-        if configProperties.get('default', 'redmine_default_activity_id') is not None:
-            activity_id = configProperties.get('default', 'redmine_default_activity_id')
-        else:
-            activity_id = None
         myxml.append('<time_entry><issue_id>{issue_id}</issue_id><spent_on>{date}</spent_on><hours>{duration}</hours><comments>{comment}</comments><activity_id>{activity_id}</activity_id></time_entry>'.format(
+                date=date,
                 issue_id=time_entry['issue_id'],
                 duration=time_entry['duration'],
-                comment=time_entry['comment'],
-                activity_id=activity_id,
-                date=date
+                comment=empty_if_none(time_entry['comment']),
+                activity_id=empty_if_none(time_entry['activity_id'])
             )
         )
     return myxml
@@ -95,6 +131,7 @@ def syncToRedmine(time_entries, sync_date):
         myredmine = Redmine(redmine_url, configProperties.get('redmine', 'key'))
     except:
         print("\nCannot connect to Redmine, check out credentials or connectivity")
+        return
     else:
         print('[OK]')
         print_("-> Sending entries")
@@ -124,11 +161,6 @@ if __name__ == '__main__':
     if not time_entries:
         print("\nNo time entries to send... have you been lazy?")
         sys.exit()
-
-    # Print entries found for that day :
-    for time_entry in time_entries:
-        print u"* [{duration}h] #{id} : {label}".format(
-            duration=time_entry['duration'], id=time_entry['issue_id'], label=time_entry['label'])
 
     print "\nPress ENTER to synchronize those tasks ...",
     try:
